65bad27a7e05f6f7dea2bd7c0ee5502f1d9d9497
Shofi <shofiyullohkamil01@gmail.com>
2025-11-06 08:07:58 +0700

    feat2: learned string

---
+++ string.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(len("kale morales")) // menghitung panjang string
+	fmt.Println("kale morales"[0]) // "string"[0] mengakses byte index ke 0
+	fmt.Println("kale morales"[1]) // "string"[1] mengakses byte index ke 1
+}

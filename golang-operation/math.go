10ca05f48bfad89a3d521f4b5eb51d0e264ca19b
Shofi <shofiyullohkamil01@gmail.com>
2025-11-06 09:23:10 +0700

    feat2: learned math operation

---
+++ golang-operation/math.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := 3
+
+	// penjumlahan
+	sum := a + b
+	fmt.Println("Penjumlahan:", sum)
+
+	// pengurangan
+	diff := a - b
+	fmt.Println("Pengurangan:", diff)
+
+	// perkalian
+	product := a * b
+	fmt.Println("Perkalian:", product)
+
+	// pembagian
+	quotient := a / b
+	fmt.Println("Pembagian:", quotient)
+
+	// sisa bagi
+	remainder := a % b
+	fmt.Println("Sisa Bagi:", remainder)
+}

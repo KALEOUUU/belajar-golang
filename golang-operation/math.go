94ccdac3931be2e7046f16a49bf28a46d3409f5b
Shofi <shofiyullohkamil01@gmail.com>
2025-11-06 14:08:44 +0700

    feat2: learned math operation 2

--- golang-operation/math.go
+++ golang-operation/math.go
@@ -25,4 +25,22 @@ func main() {
 	// sisa bagi
 	remainder := a % b
 	fmt.Println("Sisa Bagi:", remainder)
+
+	sum += 5
+	fmt.Println("Setelah Penjumlahan dengan 5:", sum)
+
+	sum -= 2
+	fmt.Println("Setelah Pengurangan dengan 2:", sum)
+
+	sum *= 3
+	fmt.Println("Setelah Perkalian dengan 3:", sum)
+
+	sum /= 4
+	fmt.Println("Setelah Pembagian dengan 4:", sum)
+
+	sum %= 3
+	fmt.Println("Setelah Sisa Bagi dengan 3:", sum)
+
+	sum ++
+	fmt.Println("Setelah Increment:", sum)
 }

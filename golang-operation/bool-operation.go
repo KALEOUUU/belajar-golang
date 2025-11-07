248ceb130098401d8ddc30a01a6bc82a6bbe135b
Shofi <shofiyullohkamil01@gmail.com>
2025-11-07 13:55:08 +0700

    feat3: learned bool-operation

---
+++ golang-operation/bool-operation.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var mathScore = 95
+	var absence = 80
+
+	//boolean operasi AND dan OR
+
+	fmt.Println(mathScore > 80 && absence > 80) 
+	fmt.Println(mathScore > 80 || absence > 80)
+}
